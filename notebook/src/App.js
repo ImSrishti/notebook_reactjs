8a6c7ccf4a9cb3f1a88ec6aa24979386aa344c42
ImSrishti <shristi998@gmail.com>
2020-09-09 18:32:37 +0530

    Initial Designing done

---
+++ notebook/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+         Hello this is me 
+      </div>
+      <div className="note_body">
+          {/* <Notestitle/>
+          <Noteslist/> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
